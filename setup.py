4b6fb3b4672877ef1ef27348c1f6322154a78247
Jiarui Xu <xvjiarui0826@gmail.com>
2022-07-26 21:12:44 -0700

    add setup.py for easy install

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+# setup(
+#     name='blip',
+#     python_requires='>=3.6, <4',
+#     packages=find_packages(exclude=('docs', 'test', 'examples')),
+#     install_requires=[
+#         "fairscale>=0.4.4"
+#     ],
+#     zip_safe=False)
+
+setup(
+    name="blip",
+    py_modules=["blip"],
+    version="1.0",
+    description="",
+    author="Salesforce",
+    packages=find_packages(include=["blip"]),
+    install_requires=[
+        "fairscale>=0.4.4"
+    ],
+    include_package_data=False,
+)
